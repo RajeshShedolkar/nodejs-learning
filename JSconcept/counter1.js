03049c8bd325023e982b5fcd60c385ad8f8d26aa
Rajesh Murlidhar Shedolkar <rajeshshedolkar12@gmail.com>
2024-05-17 21:52:54 +0530

    js questions

---
+++ JSconcept/counter1.js
@@ -0,0 +1,5 @@
+var counter = 0
+setInterval(function(){
+    counter += 1;
+    console.log(counter);
+}, 1000)
---
+++ JSconcept/counter2.js
@@ -0,0 +1,9 @@
+var counter = 0;
+
+function increment(){
+    counter += 1;
+    console.log(counter);
+    setTimeout(increment, 1000)
+}
+
+increment()
