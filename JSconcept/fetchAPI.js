ce3b0781525025e66d5fc96334a25c863aedfa58
Rajesh Shedolkar <7788754+RajeshShedolkar@users.noreply.github.com>
2024-07-01 22:08:43 +0530

    Create fetchAPI.js

---
+++ JSconcept/fetchAPI.js
@@ -0,0 +1,24 @@
+function fetchData(callback) {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://api.example.com/data');
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      callback(null, JSON.parse(xhr.responseText));
+    } else {
+      callback(new Error('Failed to fetch data'));
+    }
+  };
+  xhr.send();
+}
+
+function handleData(error, data) {
+  if (error) {
+    console.error('Error:', error);
+  } else {
+    console.log('Data:', data);
+    // Process the data here
+  }
+}
+
+// Fetch data and handle it with the callback
+fetchData(handleData);
