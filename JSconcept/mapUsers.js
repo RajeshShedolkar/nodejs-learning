23d960288d2eea7471160627385c99cfbee4100b
Rajesh Murlidhar Shedolkar <rajeshshedolkar12@gmail.com>
2024-08-08 22:17:20 +0530

    adding ma example

---
+++ JSconcept/mapUsers.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+// Read the users data from users.json file
+fs.readFile('users.json', 'utf8', (err, data) => {
+  if (err) {
+    console.error('Error reading the file:', err);
+    return;
+  }
+
+  // Parse the JSON data
+  const users = JSON.parse(data);
+
+  // Function to add fullName property to each user
+  function addFullName(users) {
+    return users.map(user => ({
+      ...user,
+      fullName: `${user.firstName} ${user.lastName}`
+    }));
+  }
+
+  // Get the transformed users
+  const transformedUsers = addFullName(users);
+
+  // Print the transformed users
+  console.log('Transformed Users:', transformedUsers);
+});
