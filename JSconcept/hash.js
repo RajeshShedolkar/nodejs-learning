19297ed8c1c79ef5417e9a9bc56a43b552dcf45c
Rajesh Murlidhar Shedolkar <rajeshshedolkar12@gmail.com>
2024-08-11 00:37:12 +0530

    updated hash file

--- JSconcept/hash.js
+++ JSconcept/hash.js
@@ -1,1 +1,4 @@
-buntissssssssssssssssssssssssssssss
+const crypto = require("crypto")
+const input = "Hello World!!!"
+const hash = crypto.createHash('sha256').update(input).digest("hex")
+console.log(hash)
