bff9d3c91cc61858cc988d40a321bd09236248af
Rajesh Murlidhar Shedolkar <rajeshshedolkar12@gmail.com>
2024-08-11 11:10:16 +0530

    hash with prefix

--- JSconcept/hash.js
+++ JSconcept/hash.js
@@ -1,4 +1,14 @@
 const crypto = require("crypto")
 const input = "Hello World!!!"
 const hash = crypto.createHash('sha256').update(input).digest("hex")
-console.log(hash)
+console.log(hash);
+var i = 1;
+var flag = true;
+while (flag){
+    var hash1 = crypto.createHash('sha256').update(i.toString()).digest("hex")
+    if (hash1.startsWith("00000")){
+        console.log(hash1 , i)
+        flag = false
+    }
+    i+=1
+}
