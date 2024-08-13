238fbe594887ea5cf1b4928e93866c819abeadc8
Rajesh Murlidhar Shedolkar <rajeshshedolkar12@gmail.com>
2024-08-14 00:36:24 +0530

    minor change

--- JSconcept/getHashNounce.js
+++ JSconcept/getHashNounce.js
@@ -1,7 +1,4 @@
 const crypto = require("crypto")
-// const input = "Hello World!!!"
-// const hash = crypto.createHash('sha256').update(input).digest("hex")
-// console.log(hash);
 var i = 1;
 var flag = true;
 let input = `
@@ -9,9 +6,9 @@ Rajesh => Banti | Rs 100
 Rishit => Dipu | Rs 10
 `
 while (flag){
-    var hash1 = crypto.createHash('sha256').update(input+i.toString()).digest("hex")
-    if (hash1.startsWith("00000")){
-        console.log(hash1 , i)
+    var hash = crypto.createHash('sha256').update(input+i.toString()).digest("hex")
+    if (hash.startsWith("00000")){
+        console.log(hash , i)
         flag = false
     }
     i+=1
