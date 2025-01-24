11c59305f5d041d7cedb24b4dedf46335eebc642
Rajesh Shedolkar <7788754+RajeshShedolkar@users.noreply.github.com>
2025-01-24 23:16:10 +0530

    Create reverseLL.js

---
+++ DSA/reverseLL.js
@@ -0,0 +1,35 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    add(value) {
+        const newNode = new Node(value);
+        if (!this.head) {
+            this.head = newNode;
+        } else {
+            let current = this.head;
+            while (current.next) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    reverse() {
+        // Implement the reverse logic here
+    }
+}
+
+const list = new LinkedList();
+list.add(1);
+list.add(2);
+list.add(3);
+list.reverse(); // Should reverse the linked list
